e619fa6830481454102f6c7e334a2e23492d3c90
Timo Bergerbusch <timo.bergerbusch@rwth-aachen.de>
2016-09-24 17:39:23 +0200

    Thread implemented GameLoop implemented

--- src/tilegame/Game.java
+++ src/tilegame/Game.java
@@ -2,16 +2,60 @@ package tilegame;
 
 import Display.Display;
 
-public class Game {
+public class Game implements Runnable {
 
     private Display display;
-
     private int width, height;
+    private String title;
+
+    private boolean running = false;
+    private Thread thread;
 
     public Game(String title, int width, int height) {
+        this.title = title;
         this.width = width;
         this.height = height;
+    }
 
+    private void init() {
         display = new Display(title, width, height);
+    }
+
+    private void render() {
+
+    }
+
+    private void tick() {
+    }
+
+    @Override
+    public void run() {
+        init();
+
+        while (running) {
+            tick();
+            render();
+        }
+
+        stop();
+    }
+
+    public synchronized void start() {
+        if (running)
+            return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    private synchronized void stop() {
+        if (!running)
+            return;
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
